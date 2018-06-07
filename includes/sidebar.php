4bfd735aed1de3c991778ffef972411a4fb9922c
marktothcode <mark-toth@outlook.com>
2018-06-07 08:15:27 +0100

    Minor changes

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -28,13 +28,13 @@
                 <div class="well">
 
                 <?php 
-                
+
                         $query = "SELECT * FROM categories";
                         $select_all_categories_sidebar = mysqli_query($connection, $query);
                         ?>
                     <h4>Blog Categories</h4>
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="list-unstyled">
                             
                         <?php while($row = mysqli_fetch_assoc($select_all_categories_sidebar)) {
@@ -42,21 +42,7 @@
                         echo "<li><a href='#'>{$cat_title}</a></li>";                
                         } ?>   
                             </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
+                        </div>                    
                     </div>
                     <!-- /.row -->
                 </div>
