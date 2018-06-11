76c0a4be0db27e16b2370ccac9970279be539507
marktothcode <mark-toth@outlook.com>
2018-06-11 07:38:34 +0100

    Refactoring into include files

--- includes/navigation.php
+++ includes/navigation.php
@@ -28,6 +28,10 @@
                 }        
                              
                 ?>
+
+                <li>
+                    <a href="admin">admin</a>
+                </li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
